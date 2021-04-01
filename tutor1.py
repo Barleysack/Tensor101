29f39330207836e9c80072dd275551eca79cfc37
Barleysack <hookypooky@naver.com>
2021-04-01 11:22:10 +0900

    TFLITE_MIC_MINI

---
+++ tutor1.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+a = tf.placeholder("float")
+b = tf.placeholder("float")
+
+y = tf.multiply(a,b)
+
+sess = tf.session()
+print sess.run(y, feed_dict=
+
+##함수 관련 도장 클리어 후 재방문 요망
+##클래스/모듈도.
