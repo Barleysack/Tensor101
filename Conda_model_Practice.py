6537d321f568188671da888c3384591bdb9f967a
Barleysack <hookypooky@naver.com>
2021-04-12 20:01:07 +0900

    Revert "lost files re-committed"
    This reverts commit d413fd52da0865ca718e07b98d10da10b99f9643.

--- Conda_model_Practice.py
+++ Conda_model_Practice.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import tensorflow as tf
 import keras
@@ -11,18 +10,4 @@ model = keras.models.Sequential([
     keras.layers.Dense(30, activation="relu", input_shape=[8]),
     keras.layers.Dense(30, activation="relu"),
     keras.layers.Dense(1)
-=======
-import numpy as np
-import tensorflow as tf
-import keras
-
-
-np.random.seed(42)
-tf.random.set_seed(42)
-
-model = keras.models.Sequential([
-    keras.layers.Dense(30, activation="relu", input_shape=[8]),
-    keras.layers.Dense(30, activation="relu"),
-    keras.layers.Dense(1)
->>>>>>> 3b5ad9f094c4e08a23bbd39c791d00a4797c73a6
 ])
