627b21fdec52b16eb6dbced50abf2236218b3162
Barleysack <hookypooky@naver.com>
2021-04-14 21:08:19 +0900

    low-level TF API& Custom Loss Function

---
+++ Hansonml2_py/Customlossfunc.py
@@ -0,0 +1,44 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras
+import tensorflow as tf
+import tensorboard
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split( housing.data, housing.target.reshape(-1, 1), random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train_full, y_train_full, random_state=42)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_valid_scaled = scaler.transform(X_valid)
+X_test_scaled = scaler.transform(X_test)
+
+
+def huber_fn(y_true, y_pred):
+    error = y_true - y_pred
+    is_small_error = tf.abs(error) < 1
+    squared_loss = tf.square(error) / 2
+    linear_loss  = tf.abs(error) - 0.5
+    return tf.where(is_small_error, squared_loss, linear_loss)
+
+
+
+
+input_shape = X_train.shape[1:]
+
+model = keras.models.Sequential([
+    keras.layers.Dense(30, activation="selu", kernel_initializer="lecun_normal",
+                       input_shape=input_shape),
+    keras.layers.Dense(1),
+])
+
+model.compile(loss=huber_fn, optimizer="nadam", metrics=["mae"])
+
+
+model.fit(X_train_scaled, y_train, epochs=2,
+          validation_data=(X_valid_scaled, y_valid))
