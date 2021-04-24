55ddedf275f93e6b96ed19cf65ca07835ad92859
Barleysack <hookypooky@naver.com>
2021-04-24 15:29:13 +0900

    Added Several modules to work

--- Handsonml2_py/VariationalAE.py
+++ Handsonml2_py/VariationalAE.py
@@ -14,6 +14,28 @@ def plot_image(image):
   plt.imshow(image, cmap="binary")
   plt.axis("off")
 
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.fashion_mnist.load_data() #MNIST 데이터 사용
+X_train_full = X_train_full.astype(np.float32) / 255
+X_test = X_test.astype(np.float32) / 255
+X_train, X_valid = X_train_full[:-5000], X_train_full[-5000:]
+y_train, y_valid = y_train_full[:-5000], y_train_full[-5000:]
+
+def rounded_accuracy(y_true, y_pred):
+    return keras.metrics.binary_accuracy(tf.round(y_true), tf.round(y_pred))
+
+def show_reconstructions(model, images=X_valid, n_images=5):
+    reconstructions = model.predict(images[:n_images])
+    fig = plt.figure(figsize=(n_images * 1.5, 3))
+    for image_index in range(n_images):
+        plt.subplot(2, n_images, 1 + image_index)
+        plot_image(images[image_index])
+        plt.subplot(2, n_images, 1 + n_images + image_index)
+        plot_image(reconstructions[image_index])
+    return plt.show()
+
+
+    
 #확률적 오토인코더이다. 훈련이 끝나도 출력이 부분적으로 우연에 의해 결정된다. 
 #생성 오토인코더이다. 훈련세트에서 샘플링 된것같은 새로운 샘플을 생성할 수 있다.
 
@@ -63,4 +85,3 @@ history = variational_ae.fit(X_train, X_train, epochs=25, batch_size=128,
                              #키워드: 변이형 오토인코더. 
                              #하지만 요새는 이거 안쓴다. 대신 GAN을 씁시다.
                              #내일은 GAN을!
-                             
