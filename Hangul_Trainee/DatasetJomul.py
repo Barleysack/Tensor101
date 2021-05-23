f89f748217842b34356bee68b67d6ca714c5d549
Barleysack <hookypooky@naver.com>
2021-05-23 23:34:48 +0900

    Moving on to next book

--- Hangul_Trainee/DatasetJomul.py
+++ Hangul_Trainee/DatasetJomul.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 
-base_dir= 'C:/Users/admin/Desktop/workspace/tensor101/data/'
+base_dir= 'C:/Users/finally/Desktop/works/workspace/data/'
 
  
 
@@ -27,3 +27,7 @@ for f in sorted(os.listdir(base_dir)):
 print(X)
 print(y)
 
+dataset=tf.data.Dataset.from_tensor_slices(X)
+#생각해보니 굳이 이렇게까지 할 필요 없을 것 같다.
+#딥러닝 컴퓨터 비전 책을 활용해서 다시 한번 도전해보자.
+#YOLO 관해서만 알아봐도 괜찮을 듯.
